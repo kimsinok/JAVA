c3a3c171e4882bf0c251085ad24b2df00e7f3f88
kimsinok <javaenjoy0714@gmail.com>
2022-07-05 09:40:21 +0900

    배열을 이용한 학생성적프로그램입니다.

---
+++ scoreApp/Student.java
@@ -0,0 +1,110 @@
+package testApp;
+
+public class Student {
+
+	// 인스턴스 변수
+	private int no;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int total;
+	private double avg;
+	private int rank;
+
+	public Student() {
+		super();
+		this.rank = 1;
+	}
+
+	public Student(int no, String name, int kor, int eng, int math) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.total = kor + eng + math;
+		this.avg = this.total / 3.0;
+		this.rank = 1;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public void printStudent() {
+		System.out.printf("%d\t %s\t %d\t %d\t %d\t %d\t %.2f\t %d%n", no, name, kor, eng, math, total, avg, rank);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
---
+++ scoreApp/StudentScoreApp.java
@@ -0,0 +1,177 @@
+package testApp;
+
+import java.util.Scanner;
+
+public class StudentScoreApp {
+
+	private static Student[] students = new Student[5];
+	
+	private static int count = 0;
+	
+	
+	// 전체 학생 성적을 조회하다.
+	public static void retrieveStudentAll() {		
+		for (int i = 0; i < count ; i++) {
+			students[i].printStudent();
+		}
+	}
+	
+	
+	// 순위를 구한다.
+	public static void rank() {
+		for (int i = 0; i < count; i++) {
+			int rank = 1;
+			for (int j = 0; j < count; j++) {
+				if(students[i].getTotal() < students[j].getTotal()) {
+					students[i].setRank(++rank);
+				}
+			}
+		}		
+	}
+	
+	
+	/*
+	 * 번호로 학생정보를 조회한다.
+	 * 번호에 해당하는 학생정보가 존재하는 경우 학생 정보를 참조하는 주소를 반환하고 
+	 * 그렇지 않은 경우 null을 반환한다.
+	 */
+	public static Student findStudentByNo(int no) {
+		for (int i = 0; i < count ; i++) {
+			if (students[i].getNo() == no) {
+				return students[i];
+			}	
+		}
+		return null;
+	}
+	
+	
+	
+	
+	/*
+ 	 * 이름으로 학생정보를 검색한다.
+	 * 동명이인이 존재할 수 있다.
+	 */	
+	public static void findStudentByName(String name) {
+		boolean isExist = false;
+		for (int i = 0; i < count ; i++) {
+			if (students[i].getName().equals(name)) {
+				students[i].printStudent();
+				isExist = true;
+			}	
+		}
+		
+		if(!isExist) {
+			System.out.println("학생정보가 존재하지 않습니다. 정확한 이름을 입력하세요.");
+		}
+	}
+	
+	
+	/*
+	 * 학생정보를 검색하다
+	 * 1 : 번호, 2 : 이름, 3 : 종료
+	*/
+	public static void findStudent() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+
+			System.out.println("�꽦�쟻 寃��깋");
+			System.out.println("1. 踰덊샇");
+			System.out.println("2. �씠由�");
+			System.out.println("3. 醫낅즺");
+			System.out.print("�빆紐⑹쓣 �꽑�깮�븯�꽭�슂 ");
+			int item = Integer.parseInt(scan.nextLine());	
+			
+			if (item == 1) {
+				System.out.print("踰덊샇 : ");
+				int no = Integer.parseInt(scan.nextLine());	
+				Student s = findStudentByNo(no);
+				if (s == null) {
+					continue;
+				}				
+				s.printStudent();
+				
+			} else if (item == 2) {
+				System.out.print("�씠由� : ");
+				String name = scan.nextLine();				
+				findStudentByName(name);				
+						
+			} else {
+				break;
+			}
+		
+		} while (true);
+
+	}
+	
+
+	/*
+	 * 학생 성적을 등록하다
+	 * 번호, 이름, 국어, 영어, 수학 
+	 */
+	public static void regiterStudent() {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("번호 : ");
+		int no = Integer.parseInt(scan.nextLine());
+
+		System.out.print("이름 : ");
+		String name = scan.nextLine();
+
+		System.out.print("국어 : ");
+		int kor = Integer.parseInt(scan.nextLine());
+
+		System.out.print("영어 : ");
+		int eng = Integer.parseInt(scan.nextLine());
+
+		System.out.print("수학 : ");
+		int math = Integer.parseInt(scan.nextLine());
+
+		students[count] = new Student(no, name, kor, eng, math);
+		count++;
+		rank();
+		System.out.println("학생정보가 등록되었습니다.");
+
+	}
+	
+
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		boolean isExit = false;
+		do {
+
+			System.out.println("===== 성적관리시스템 =====");
+			System.out.println("1. 성적 등록");
+			System.out.println("2. 성적 검색");
+			System.out.println("3. 전체 성적 조회");
+			System.out.println("4. 종료");
+			System.out.print("항목을 선택하세요 ");
+
+			int item = Integer.parseInt(scan.nextLine());
+			
+						
+			switch (item) {
+			case 1:
+				regiterStudent();
+				break;
+			case 2:
+				findStudent();
+				break;
+			case 3:
+				retrieveStudentAll();
+				break;
+			case 4:
+				isExit = true;
+				break;
+			}
+
+		} while (!isExit);
+
+	}
+
+}
